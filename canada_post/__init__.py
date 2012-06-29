07e7e0ee70205ee979927fa440f96746ab89e73f
Tomas Neme <lacrymology@gmail.com>
2012-06-29 16:02:13 -0300

    some settings (unused as yet)

--- canada_post/__init__.py
+++ canada_post/__init__.py
@@ -1,1 +1,48 @@
 __author__ = 'lacrymology'
+"""
+Configuration module for the API. This defines whether to use the development
+or production credentials/urls (and what the credentials are).
+"""
+
+DEBUG = False
+
+DEV = "DEV"
+PROD = "PROD"
+
+USERNAME = ""
+PASSWORD = ""
+
+class Auth(object):
+    def __init__(self, username="", password="", dev=PROD):
+        self.USERNAME = {
+            DEV: "",
+            PROD: "",
+            }
+        self.USERNAME[dev]=username
+        self.PASSWORD = {
+            DEV: "",
+            PROD: "",
+        }
+        self.PASSWORD[dev] = password
+
+    def set_password(self, password, dev=PROD):
+        """
+        Set the password.
+
+        @parameter
+        `dev`: Whether this is the development or production password. Should
+                be canada_post.DEV or canada_post.PROD
+        """
+        self.PASSWORD[dev] = password
+
+    def set_username(self, username, dev=PROD):
+        """
+        Set the username.
+
+        @parameter
+        `dev`: Whether this is the development or production password. Should
+                be canada_post.DEV or canada_post.PROD
+        """
+        self.USERNAME[dev] = username
+
+AUTH = Auth()
