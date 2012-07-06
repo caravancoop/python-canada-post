119102ede37c46f7a763fd4bec5ed0adf05b19ab
Tomas Neme <lacrymology@gmail.com>
2012-07-06 20:29:42 -0300

    make postal code compatible (no spaces)

--- canada_post/util/address.py
+++ canada_post/util/address.py
@@ -1,7 +1,12 @@
 from canada_post.util import InfoObject
 
-class Origin(InfoObject):
+class AddressBase(InfoObject):
+    def __init__(self, postal_code, **kwargs):
+        self.postal_code = postal_code.replace(" ", "")
+        super(InfoObject, self).__init__(**kwargs)
+
+class Origin(AddressBase):
     pass
 
-class Destination(InfoObject):
+class Destination(AddressBase):
     pass
