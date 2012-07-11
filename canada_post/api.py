f4129daa534004411529855ad0a3e4ebe3570e82
Tomas Neme <lacrymology@gmail.com>
2012-07-11 13:56:52 -0300

    prepare API object for client usage

--- canada_post/api.py
+++ canada_post/api.py
@@ -1,8 +1,10 @@
 """
 Central API module
 """
-from lxml import etree
+from canada_post import PROD, Auth
+from canada_post.service.rating import (GetRatesClass)
 
 class CanadaPostAPI(object):
-    def __init__(self):
-        self.get_rates = None
+    def __init__(self, customer_number, username, password, dev=PROD):
+        self.auth = Auth(customer_number, username, password, dev)
+        self.get_rates = GetRatesClass(self.auth)
