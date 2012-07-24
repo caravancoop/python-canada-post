cf08eb66d6c345f37e32b57e56625eba8f425f54
Tomas Neme <lacrymology@gmail.com>
2012-07-24 18:18:17 -0300

    add some new fields to the address based, needed in the CreateShipping method

--- canada_post/util/address.py
+++ canada_post/util/address.py
@@ -1,10 +1,23 @@
 from canada_post.util import InfoObject
 
 class AddressBase(InfoObject):
-    def __init__(self, postal_code, name=None, company=None, **kwargs):
+    def __init__(self, postal_code, name=None, company=None, phone=None,
+                 address=None, city=None, province=None,
+                 *args, **kwargs):
         self.postal_code = postal_code.replace(" ", "")
         self.name = name
         self.company = company
+        self.phone = phone
+
+
+        self.city = city
+        if city:
+            assert len(city) < 40, ("City name must have up to 40 characters")
+
+        self.province = province
+
+        assert not args, "Too many positional arguments for {} object".format(
+            self.__class__.__name__)
         super(AddressBase, self).__init__(**kwargs)
 
 class Origin(AddressBase):
