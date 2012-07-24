2ebe11c302174b2adc9786f2039343cf9c5f9305
Tomas Neme <lacrymology@gmail.com>
2012-07-24 15:35:48 -0300

    new required and optional parameters for address objects

--- canada_post/util/address.py
+++ canada_post/util/address.py
@@ -1,14 +1,18 @@
 from canada_post.util import InfoObject
 
 class AddressBase(InfoObject):
-    def __init__(self, postal_code, **kwargs):
+    def __init__(self, postal_code, name=None, company=None, **kwargs):
         self.postal_code = postal_code.replace(" ", "")
+        self.name = name
+        self.company = company
         super(AddressBase, self).__init__(**kwargs)
 
 class Origin(AddressBase):
-    pass
+    def __init__(self, phone=None, *args, **kwargs):
+        self.phone = phone
+        super(Origin, self).__init__(*args, **kwargs)
 
 class Destination(AddressBase):
-    def __init__(self, postal_code, country_code, **kwargs):
+    def __init__(self, country_code, *args, **kwargs):
         self.country_code = country_code
-        super(Destination, self).__init__(postal_code=postal_code, **kwargs)
+        super(Destination, self).__init__(*args, **kwargs)
