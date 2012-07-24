3caec61019b58b1da85df326c997d51a85ae7bbe
Tomas Neme <lacrymology@gmail.com>
2012-07-24 18:19:20 -0300

    add contract_number to API init for CreateShipping

--- canada_post/api.py
+++ canada_post/api.py
@@ -5,6 +5,8 @@ from canada_post import PROD, Auth
 from canada_post.service.rating import (GetRates)
 
 class CanadaPostAPI(object):
-    def __init__(self, customer_number, username, password, dev=PROD):
-        self.auth = Auth(customer_number, username, password, dev)
+    def __init__(self, customer_number, username, password, contract_number="",
+                 dev=PROD):
+        self.auth = Auth(customer_number, username, password, contract_number,
+                         dev)
         self.get_rates = GetRates(self.auth)
